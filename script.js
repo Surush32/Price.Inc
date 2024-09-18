3951b3fd64f1d810f70820cb9233833d9e7be64c
Surush32 <Surush32@icloud.com>
2024-09-18 17:42:32 -0600

    project started
    Home, About, Services sections are looking good. Once the client see the website we would consider their requests if changes are needed.

---
+++ script.js
@@ -0,0 +1,5 @@
+// JavaScript to toggle the navigation menu
+document.getElementById("hamburger").onclick = function () {
+    var navLinks = document.getElementById("nav-links");
+    navLinks.classList.toggle("show");
+};
