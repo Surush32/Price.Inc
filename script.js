7733c76e8f9aa0b558278a6602d57b29d13a0732
Surush32 <Surush32@icloud.com>
2024-09-19 14:35:39 -0600

    changed the service section image.

--- script.js
+++ script.js
@@ -21,3 +21,11 @@ leftArrow.onclick = function () {
     });
 };
 
+const hamburger = document.getElementById('hamburger');
+const navLinks = document.getElementById('nav-links');
+
+// Toggle the show class when the hamburger menu is clicked
+hamburger.onclick = function () {
+    navLinks.classList.toggle('show');
+};
+
